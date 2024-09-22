ce6ac9523d902e7b7cf6100e793c35c2eae9de57
KetanKansal <ketankansal124@gmail.com>
2024-09-22 10:21:00 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+#setuptools is a package that provide tools for packaging python projects
+#find_packages find the packages in your project
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT="-e."
+def get_requirements(file_path: str) -> List[str]:
+    """Reads the requirements.txt file and returns a list of dependencies."""
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+setup(
+    name="Fault Detection",
+    version="0.0.1",
+    author="Ketan Kansal",
+    author_email="ketankansal124@gmail.com",
+    install_requires=get_requirements("requirements.txt"), 
+    packages=find_packages()
+)
